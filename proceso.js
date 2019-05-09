ce616e0aa233806d606e0c032ca504f1608dae31
RIcardoVinicioJaraJara <49212955+RIcardoVinicioJaraJara@users.noreply.github.com>
2019-05-09 07:47:16 -0500

    Metodos 1

--- proceso.js
+++ proceso.js
@@ -1,7 +1,19 @@
+var numAux = 0;
 function numero(num){
     if( document.getElementById("pantalla").value != '0'){
     document.getElementById("pantalla").value=(document.getElementById("pantalla").value+ num);
     }else{
         document.getElementById("pantalla").value=(num);
+    }
+}
+
+function borrar() {
+    var num = document.getElementById("pantalla").value
+    if(num.length <= 1){
+        document.getElementById("pantalla").value=("0");
+        numAux = 0;
+
+    }else{
+        document.getElementById("pantalla").value=(num.substr(0,num.length-1))
     }
 }
